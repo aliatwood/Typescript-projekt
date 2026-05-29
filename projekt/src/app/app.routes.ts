f6a43495042a60b16b0b0edbb8d700c5b5c0be8b
aliatwood <aliatwood0104@gmail.com>
2026-05-29 21:31:31 +0200

    la till routing och navigation mellan sidor

--- projekt/src/app/app.routes.ts
+++ projekt/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Kurser } from './sidor/kurser/kurser';
+import { Ramschema } from './sidor/ramschema/ramschema';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'kurser', pathMatch: 'full' },
+  { path: 'kurser', component: Kurser },
+  { path: 'ramschema', component: Ramschema }
+];
