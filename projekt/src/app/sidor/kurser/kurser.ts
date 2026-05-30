92518c3f5785934af3721b14dbd4afdf5679c108
aliatwood <aliatwood0104@gmail.com>
2026-05-30 14:32:12 +0200

    Skapar ny struktur
    skapar routing, courseService och kurser komponent med datahämtning

--- projekt/src/app/sidor/kurser/kurser.ts
+++ projekt/src/app/sidor/kurser/kurser.ts
@@ -1,20 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { CourseService, Course } from '../../services/course';
 
 @Component({
   selector: 'app-kurser',
+  imports: [CommonModule],
   templateUrl: './kurser.html',
-  styleUrls: ['./kurser.css']
+  styleUrl: './kurser.css',
 })
 export class Kurser implements OnInit {
-
   courses: Course[] = [];
+  filteredCourses: Course[] = [];
 
   constructor(private courseService: CourseService) {}
 
   ngOnInit(): void {
     this.courseService.getCourses().subscribe(data => {
       this.courses = data;
+      this.filteredCourses = data;
     });
   }
 }
